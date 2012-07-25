17493054b2514b6f74c9ade094956194fee92561
Raymond Chandler III <raymondchandleriii@gmail.com>
2012-07-25 01:21:34 -0400

    Added setup.py file for PyPI

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django_haml',
+        version='1.0',
+        description='A HAML pre-processor for Django 1.3 and Django 1.4',
+        author='Raymond Chandler III',
+        author_email='raymondchandleriii@gmail.com',
+        url='https://github.com/kitanata/django_haml',
+        packages=['django_haml'],
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: MacOS X',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'License :: OSI Approved :: Python Software Foundation License',
+            'Natural Language :: English',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows :: Windows 7',
+            'Operating System :: Microsoft :: Windows :: Windows Vista',
+            'Operating System :: Microsoft :: Windows :: Windows XP',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2.7'
+        ], license = 'http://opensource.org/licenses/python-2.0',
+        )
